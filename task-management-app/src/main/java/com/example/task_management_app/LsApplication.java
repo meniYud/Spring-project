952a69c1f7529c5ab10d95a200de35788ba36637
meniYud <menyud@gmail.com>
2024-07-13 15:31:27 +0300

    feat: add persistence layer

--- task-management-app/src/main/java/com/example/task_management_app/LsApplication.java
+++ task-management-app/src/main/java/com/example/task_management_app/LsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskManagementAppApplication {
+public class LsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskManagementAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LsApplication.class, args);
+    }
 
 }
